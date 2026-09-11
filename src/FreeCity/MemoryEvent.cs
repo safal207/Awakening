9295f30b6ae30fd8877e27f6af7dc5260c01221b
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-09-12 00:53:01 +0700

    feat: bind memory event to explicit participants

--- src/FreeCity/MemoryEvent.cs
+++ src/FreeCity/MemoryEvent.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+
 namespace Probuzhdenie.FreeCity;
 
 public sealed class MemoryEvent
 {
     public string EventId { get; set; } = "";
     public string EventType { get; set; } = "";
+    public List<int> ParticipantIds { get; set; } = new();
     public string LocationId { get; set; } = "";
     public string ChoiceId { get; set; } = "";
     public int Day { get; set; }
