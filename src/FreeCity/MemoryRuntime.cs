02913c8687fb4501257125ef433677203e0442d9
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-09-12 01:08:50 +0700

    feat: expose active hero and last Sverka report

--- src/FreeCity/MemoryRuntime.cs
+++ src/FreeCity/MemoryRuntime.cs
@@ -5,14 +5,25 @@ public static class MemoryRuntime
     private static MemoryLedger _current = new();
 
     public static MemoryLedger Current => _current;
+    public static int HeroId { get; set; }
+    public static SverkaReport? LastSverkaReport { get; private set; }
 
     public static void Replace(MemoryLedger? ledger)
     {
         _current = ledger ?? new MemoryLedger();
+        LastSverkaReport = null;
+    }
+
+    public static SverkaReport RunSverka(int day)
+    {
+        LastSverkaReport = SverkaEngine.Run(_current, HeroId, day);
+        return LastSverkaReport;
     }
 
     public static void Reset()
     {
         _current = new MemoryLedger();
+        HeroId = 0;
+        LastSverkaReport = null;
     }
 }
