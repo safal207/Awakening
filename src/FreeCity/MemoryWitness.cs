94b75b7b38197e29cf63777b4afa80675995659b
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-09-12 00:53:06 +0700

    feat: require witness understanding

--- src/FreeCity/MemoryWitness.cs
+++ src/FreeCity/MemoryWitness.cs
@@ -3,6 +3,7 @@ namespace Probuzhdenie.FreeCity;
 public sealed class MemoryWitness
 {
     public int NpcId { get; set; }
+    public bool UnderstoodEvent { get; set; }
     public bool Consented { get; set; }
     public string ConsentReason { get; set; } = "";
 }
